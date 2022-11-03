5217c11e7c34b151f91db2671acfda2f2a0c8eee
sai jigeesha kancharla <114378273+jigeesha-kancharla@users.noreply.github.com>
2022-11-03 20:11:36 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,14 +35,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-        System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-        System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-
+  
   }
 }
